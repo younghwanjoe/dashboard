28714954b30b5d881a4a2ee3bf723196f5d4baae
조영환 <hhgman2@gmail.com>
2020-05-16 20:51:08 +0900

    add translate api -one corresponding post '/submit' request -one corresponding post '/listLanguages' request

---
+++ backend/api/translateApi.js
@@ -0,0 +1,61 @@
+var express = require('express');
+var router = express.Router();
+
+const {Translate} = require(
+    '@google-cloud/translate'
+).v2;
+// Project id from config.json
+const config = require('../.google-cloud-config')
+const translate = new Translate(config);
+
+let model = 'nmt';
+
+async function translateTextWithModel(text,target) {
+
+    const options = {
+        to: target,
+        model: model
+    }
+
+    let [translations] = await translate.translate(text, options);
+    translations = Array.isArray(translations) ? translations : [translations];
+    console.log('번역:');
+    translations.forEach((translation, i) => {
+        console.log(`${text[i]} => (${target}) ${translation}`);
+    });
+    return {
+        target:target,
+        text:text,
+        translation: translations[0]
+    }
+}
+
+async function listLanguages() {
+  // Lists available translation language with their names in English (the default).
+  const [languages] = await translate.getLanguages();
+
+  console.log("Languages:");
+  let return_array = []
+  languages.forEach((language) =>
+  return_array.push(language)
+    );
+  return return_array;
+  
+}
+router.post("/submit", async (req, res, next)=> {
+    console.log('req',req.body)
+    const text = req.body.text
+    const target = req.body.target
+    const result = await translateTextWithModel(text,target)
+
+    res.send(result)
+});
+
+router.get("/listLanguages",async(req, res, nex) =>{
+    const result = await listLanguages();
+    res.send(result)
+})
+
+module.exports = router
+
+
